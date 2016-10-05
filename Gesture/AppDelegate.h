581ad8966cbd74d12de965c1720daced045af9c0
ExtremeOO <jack@useradmintekimacbook-pro.local>
2016-10-05 10:38:07 +0800

    Initial Commit

---
+++ Gesture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gesture
+//
+//  Created by System Administrator on 16/10/5.
+//  Copyright © 2016年 zll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Gesture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gesture
+//
+//  Created by System Administrator on 16/10/5.
+//  Copyright © 2016年 zll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
